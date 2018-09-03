83bab62eebf4a5e887094d6127b54fa7c2f3eb89
Chenying517 <Cheny_0595@163.com>
2018-09-03 10:22:28 +0800

    header组件
    Signed-off-by: Chenying517 <Cheny_0595@163.com>

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Greeter from './Greeter';
+import Header from './page/header';
 
 render(
-    <Greeter />,
+    <Header />,
     document.getElementById('root')
 );
 
---
+++ app/page/header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import logo from '../../static/img/logo'
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <img src={require('../../static/img/')} />
+                <div> React Music Player</div>
+            </div>
+        );
+    }
+}
+export default Header
+
